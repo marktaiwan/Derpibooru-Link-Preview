f573157fe2f498109720c5369edf68a65f29a23f
Mark Taiwan <mark.taiwan@gmail.com>
2016-09-22 15:47:43 +0800

    Update meta

--- derpibooru quote preview.user.js
+++ derpibooru quote preview.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Derpibooru Comment Preview
 // @description  Hover preview for links to other comments
-// @version      1.0.19
+// @version      1.0.20
 // @author       Marker
 // @namespace    https://github.com/marktaiwan/
-// @homepageURL  https://github.com/marktaiwan/Derpibooru-Quote-Preview
-// @supportURL   https://github.com/marktaiwan/Derpibooru-Quote-Preview/issues
+// @homepageURL  https://github.com/marktaiwan/Derpibooru-Link-Preview
+// @supportURL   https://github.com/marktaiwan/Derpibooru-Link-Preview/issues
 // @include      /^https?://(www\.)?(derpiboo\.ru|derpibooru\.org|trixiebooru\.org)/(images/)?\d{1,}(\?|\?.{1,}|/|\.html)?$/
 // @include      /^https?://(www\.)?(derpiboo\.ru|derpibooru\.org|trixiebooru\.org)/lists/user_comments/\d{1,}(\?|\?.{1,}|/|\.html)?$/
 // @include      /^https?://(www\.)?(derpiboo\.ru|derpibooru\.org|trixiebooru\.org)/lists/my_comments(\?|\?.{1,}|/|\.html)?$/
