85f7daa61adcb9ac0cb11f5f3706837c61508e27
Mark Taiwan <mark.taiwan@gmail.com>
2017-03-17 02:01:04 +0800

    Version bump
    v.1.2.0

--- derpibooru quote preview.user.js
+++ derpibooru quote preview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Derpibooru Comment Preview
 // @description  Hover preview for links to other comments
-// @version      1.1.0
+// @version      1.2.0
 // @author       Marker
 // @namespace    https://github.com/marktaiwan/
 // @homepageURL  https://github.com/marktaiwan/Derpibooru-Link-Preview
