025ad61bedd5aa305b5d34834197425c68c74ccd
Mark Taiwan <marktaiwan@users.noreply.github.com>
2021-03-24 15:18:39 +0800

    Fix selector for embedded images

--- comment-enhancements.user.js
+++ comment-enhancements.user.js
@@ -583,7 +583,7 @@
 
     // Add clickable links to hotlinked images
     if (LINK_IMAGES) {
-        NodeCreationObserver.onCreation('.communication__body__text .imgspoiler>img, .image-description .imgspoiler>img', img => {
+        NodeCreationObserver.onCreation('.communication__body__text img, .image-description img', img => {
             if (img.closest('a') !== null) return; // Image is already part of link so we do nothing.
 
             const imgParent = img.parentElement;
